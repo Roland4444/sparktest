3363b34b0e2c95d8e4fb72648743f890ef930f4b
Roland4444 <turbopollux@rambler.ru>
2021-01-18 13:09:10 +0400

    ready for prod

---
+++ src/main/resources/public/js/websocket.js
@@ -0,0 +1,23 @@
+if (!window.WebSocket) {
+	document.body.innerHTML = 'WebSocket в этом браузере не поддерживается.';
+}
+var socket = new WebSocket("ws://localhost:4567/echo");
+// обработчик входящих сообщений
+socket.onmessage = function(event) {
+  var incomingMessage = event.data;
+  ///addscript(incomingMessage);
+  console.log('message'+incomingMessage)
+  proccessJSON(incomingMessage)
+  alert('получен новый запрос. проверьте таблицу')
+  //location.reload();
+};
+// показать сообщение в div#subscribe
+function addscript(scripted){
+    var script = document.createElement('script');
+    script.onload = function ()
+    {
+     ////   alert('dynamical loaded');
+    };
+    script.src = alert(scripted);;;
+    document.head.appendChild(script);
+}
