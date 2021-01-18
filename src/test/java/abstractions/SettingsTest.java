ca1af831dc000dbdbb73ace4524038fa0e3aa8a1
Roland4444 <turbopollux@rambler.ru>
2021-01-18 11:44:58 +0400

    update

--- src/test/java/abstractions/SettingsTest.java
+++ src/test/java/abstractions/SettingsTest.java
@@ -12,9 +12,10 @@ public class SettingsTest {
     @Test
     public void initSetts() throws IOException {
         setts.AktorPORT="http://127.0.0.1:12121/";
-        setts.requestsPOSTGRESConnect="jdbc:postgresql://localhost/requests?user=user2&password=123";
-        setts.usersPostgresConnect="jdbc:postgresql://localhost/users?user=user2&password=123";
-        BinaryMessage.write(BinaryMessage.savedToBLOB(setts), "setts.bin");
+        setts.requestsPOSTGRESConnect="jdbc:postgresql:// 192.168.0.126/requests?user=user2&password=123";
+        setts.usersPostgresConnect="jdbc:postgresql:// 192.168.0.126/users?user=user2&password=123";
+        BinaryMessage.write(BinaryMessage.savedToBLOB(setts), "setts2.bin");
     }
+
 
 }
